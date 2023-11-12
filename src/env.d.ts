1f47a4f7abf3a6ca3e75fdeb56a1e3875b91a1dc
Jesus Vallez Gomis <jesusvg91@gmail.com>
2023-11-12 18:44:54 +0100

    feat(cms): contentful token env

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     CONTENTFUL_SPACE_ID?: string
     CONTENTFUL_MANAGEMENT_TOKEN?: string
-    CONTENTFUL_PREVIEW_TOKEN?: string
-    CONTENTFUL_DELIVERY_TOKEN?: string
+    CONTENTFUL_TOKEN?: string
+    // CONTENTFUL_PREVIEW_TOKEN?: string
+    // CONTENTFUL_DELIVERY_TOKEN?: string
   }
 }
